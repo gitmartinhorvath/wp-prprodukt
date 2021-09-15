40e9e54aa5c027ef53f936aff7db431cca2d8d94
gitmartinhorvath <info@martinhorvath.sk>
2021-09-15 13:11:52 +0200

    Add create page - contact _

---
+++ contact.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Template Name: Kontakt
+*/
+?>
+<?php get_header( 'page' ); ?>
+
+<!-- Start Item Details -->         
+<section class="item-details section co-wp-page"> 
+    <div class="container"> 
+        <div class="row"> 
+            <!-- site content -->                     
+            <div class="product-details-info col-lg-12 col-12"> 
+                <div class="single-block"> 
+                    <div class=""> 
+                        <div class="info-body custom-responsive-margin"> 
+                            <div class="co-contact">
+
+                                <style type="text/css"></style>
+                                 <!-- Start co-contact Top -->             
+            <div class="co-contact-top"> 
+                <div class="container"> 
+                    <div class="inner-content"> 
+                        <div class="row"> 
+                            <div class="col-lg-3 col-md-4 col-12"> 
+                                <div class="co-contact-logo"> 
+                                    <a href="index.html"> <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo/LOGO_PROPRODUKT.svg" alt="Logo" style=" max-height: 80px;"> </a> 
+                                </div>                                 
+                            </div>                                                          
+                        </div>                         
+                    </div>                     
+                </div>                 
+            </div>             
+            <!-- End co-contact Top -->             
+            <!-- Start co-contact Middle -->             
+            <div class="co-contact-middle"> 
+                <div class="container"> 
+                    <div class="bottom-inner"> 
+                        <div class="row"> 
+
+                             <div class="col-lg-4 col-md-6 col-12"> 
+                                <!-- Single Widget -->                                 
+                                <div class="single-co-contact f-contact"> 
+                                    <h3 class="co-dark-txt">Komunikácia v Anglickom a <br>Maďarskom jazyku</h3> 
+                                    <h5> Ildikó Faboková</h5> 
+                                    <p class="phone"><i class="lni lni-phone"></i> <?php _e( 'Tel: +421 904-419-471', 'proprodukt' ); ?></p> 
+                                    <ul> 
+                                        <li><i class="lni lni-map-marker"></i>
+                                            <span> <?php _e( 'PROPRODUKT s.r.o.', 'proprodukt' ); ?></span> 
+                                            <br><?php _e( 'Rybárska 685/53B', 'proprodukt' ); ?>
+                                            <?php _e( 'Hamuliakovo,', 'proprodukt' ); ?>
+                                            <?php _e( '90043', 'proprodukt' ); ?>
+                                        </li>                                         
+                                        <li> 
+                                            <span> <?php _e( 'IČO:50 877 062', 'proprodukt' ); ?></span> 
+                                            <span> <?php _e( 'DIČ:2120521997', 'proprodukt' ); ?></span> 
+                                        </li>                                         
+                                    </ul>                                     
+                                    <p class="mail"> <i class="lni lni-envelope"></i> <a href="mailto:info@proprodukt.sk"><?php _e( 'info@proprodukt.sk', 'proprodukt' ); ?></a> </p> 
+                                </div>                                 
+                                <!-- End Single Widget -->                                 
+                            </div> 
+
+
+
+                            <div class="col-lg-4 col-md-6 col-12"> 
+                                <!-- Single Widget -->                                 
+                                <div class="single-co-contact f-contact"> 
+                                    <h3 class="co-dark-txt"><?php _e( 'Konateľ', 'proprodukt' ); ?> </h3> 
+                                    <h5> Lukáš Fabok </h5> 
+                                    <p class="phone"><i class="lni lni-phone"></i> <?php _e( 'Tel: +421 910-999-819', 'proprodukt' ); ?></p> 
+                                    <ul> 
+                                        <li><i class="lni lni-map-marker"></i>
+                                            <span> <?php _e( 'PROPRODUKT s.r.o.', 'proprodukt' ); ?></span> 
+                                            <br><?php _e( 'Rybárska 685/53B', 'proprodukt' ); ?>
+                                            <?php _e( 'Hamuliakovo,', 'proprodukt' ); ?>
+                                            <?php _e( '90043', 'proprodukt' ); ?>
+                                        </li>                                         
+                                        <li> 
+                                            <span> <?php _e( 'IČO:50 877 062', 'proprodukt' ); ?></span> 
+                                            <span> <?php _e( 'DIČ:2120521997', 'proprodukt' ); ?></span> 
+                                        </li>                                         
+                                    </ul>                                     
+                                    <p class="mail"> <i class="lni lni-envelope"></i> <a href="mailto:info@proprodukt.sk"><?php _e( 'info@proprodukt.sk', 'proprodukt' ); ?></a> </p> 
+                                </div>                                 
+                                <!-- End Single Widget -->                                 
+                            </div> 
+                            
+                            <div class="col-lg-4 col-md-6 col-12"> 
+                                <!-- Single Widget -->                                 
+                                <div class="single-co-contact f-link"> 
+                                    <!-- cunsom -->                                     
+                                    <h3 class="co-dark-txt">OTVÁRACIE HODINY</h3> 
+                                        <style type="text/css">
+                                        .co-li-contact{
+                                                margin: 10px 0 10px 0;
+                                            } 
+                                        </style>                                
+                                     <ul>
+                                        <li class="co-li-contact">Pondelok - 8:00 – 16:30</li>
+                                        <li class="co-li-contact">Utorok - 8:00 – 16:30</li>
+                                        <li class="co-li-contact">Streda - 8:00 – 16:30</li>
+                                        <li class="co-li-contact">Štvrtok - 8:00 – 16:30</li>
+                                        <li class="co-li-contact">Piatok - 8:00 – 16:30</li>
+                                        <li class="co-li-contact">Sobota - Zatvorené</li>
+                                        <li class="co-li-contact">Nedeľa - Zatvorené</li>
+                                        
+                                    </ul>
+                                </div>                                 
+                                <!-- End Single Widget -->                                 
+                            </div>                             
+                        </div>                         
+                    </div>                     
+                </div>                 
+            </div>             
+            <!-- End co-contact Middle -->             
+               
+                               
+                            </div>                                     
+                        </div>                                 
+                    </div>                             
+                </div>                         
+            </div>                     
+            <!--End: site content -->                     
+        </div>                 
+    </div>             
+</div>                 
+
+<?php get_footer(); ?>
